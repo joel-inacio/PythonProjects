39747fa7d68811c8fc104b36b9751942cb831dd6
MrJoel <joelinaciodsouzafilho@gmail.com>
2026-07-24 08:09:23 -0300

    Initial commit

---
+++ idadefamilia.py
@@ -0,0 +1,10 @@
+#Fazer com que caso a pessoa aperte ENTER ele me dê o resultado já direto da média, sem a nescessidade de completar
+pessoa1 = float(input("Digite a idade: "))
+pessoa2 = float(input("Digite a idade: "))
+pessoa3 = float(input("Digite a idade: "))
+pessoa4 = float(input("Digite a idade: "))
+pessoa5 = float(input("Digite a idade: "))
+
+media = (pessoa1 + pessoa2 + pessoa3 + pessoa4 + pessoa5) / 2
+
+print(media)
