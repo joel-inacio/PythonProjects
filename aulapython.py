d0881748567d4185bfc457283db5154fe39d1cdb
MrJoel <joelinaciodsouzafilho@gmail.com>
2026-07-28 18:58:40 -0300

    .

---
+++ aulapython.py
@@ -0,0 +1,11 @@
+#Primeira aula de programação em Python.
+"""
+Posso usar essa função das "3 aspas JUNTAS" para comentar um bloco com várias palavras ou com uma explicação
+"""
+
+print("Hello World!") #Print é uma função usada para 'falar' alguma coisa na saída do programa. Posso colocar uma variável, ou uma string (texto).
+numero = 10 #Aqui temos uma variável com o valor 10. Se eu usar a função print(numero) na saída terei o valor dessa variável
+nome = "Joel"
+resultado = True
+resultado1 = False
+print(nome)
