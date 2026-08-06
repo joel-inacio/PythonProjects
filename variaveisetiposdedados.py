e8d7015f765e232e2b8ad6a0da292cab1943415b
Joel Inácio <114336425+joel-inacio@users.noreply.github.com>
2026-08-06 17:02:23 -0300

    Add files via upload

---
+++ variaveisetiposdedados.py
@@ -0,0 +1,12 @@
+#Atividade em Sala
+nome = 'Joel'
+idade = 20
+print(f'O {nome} tem {idade} anos.')
+anonascimento = int(input('Digite seu ano de nascimento: '))
+print(type(anonascimento))
+valor1 = int(input("Digite um número: "))
+valor2 = int(input("Digite outro número: "))
+print(valor1 + valor2)
+print(valor1 - valor2)
+print(valor1 * valor2)
+print(valor1 / valor2)
